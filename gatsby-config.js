72ebfa2a331374e94de7061872e31dddae32832b
sameeksharathi <rathisameeksha9@gmail.com>
2021-07-01 19:15:52 +0530

    data adding and pulling in data layer

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
   },
   plugins: ["gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,18 +1,31 @@
 // LAYOUT COMPONENT
 
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
     container, heading,
     navLinks,
     navLinkItem,
-    navLinkText
+    navLinkText,
+    siteTitle
 } from '../components/layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+
+    const title = useStaticQuery(graphql`
+    query {
+        site{
+            siteMetadata{
+                title
+            }
+        }
+    }`)
+
+    console.log(title);
     return (
         <main className={container}>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {title.site.siteMetadata.title}</title>
+            <p className={siteTitle} >{title.site.siteMetadata.title}</p>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}>
@@ -23,6 +36,11 @@ const Layout = ({ pageTitle, children }) => {
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>
                             About
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/blog" className={navLinkText}>
+                            Blog
                         </Link>
                     </li>
                 </ul>
